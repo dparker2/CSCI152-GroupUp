9b8a3a1085206efece7bfca4cb449b7fb7328937
unknown <crazdave@gmail.com>
2018-04-09 00:50:30 -0700

    group chat/whiteboard

---
+++ src/models/models.go
@@ -0,0 +1,55 @@
+package models
+
+import (
+	"github.com/gorilla/websocket"
+)
+
+type user struct {
+	Name   string
+	WsConn *websocket.Conn
+}
+
+type group struct {
+	Users []user
+	Name  string
+}
+
+var groups map[string]*group
+
+func Init() {
+	groups = make(map[string]*group)
+}
+
+func GroupExists(name string) bool {
+	if _, exists := groups[name]; exists {
+		return true
+	} else {
+		return false
+	}
+}
+
+// AddGroup adds a group with name
+func AddGroup(name string) {
+	groups[name] = &group{
+		Users: nil,
+		Name:  name,
+	}
+}
+
+func AddUserToGroup(name string, conn *websocket.Conn, grpName string) {
+	newUser := user{
+		Name:   name,
+		WsConn: conn,
+	}
+	currentUsers := groups[grpName].Users
+	users := append(currentUsers, newUser)
+	groups[grpName].Users = users
+}
+
+func GetConnectionsInGroup(grpName string) (conn []*websocket.Conn) {
+	users := groups[grpName].Users
+	for _, user := range users {
+		conn = append(conn, user.WsConn)
+	}
+	return
+}
